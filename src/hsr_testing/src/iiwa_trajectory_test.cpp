3ffe43a821fbe5252ea80aab37dfd48ebddbbf91
Alexander Saikia <alexander.saikia.21@ucl.ac.uk>
2022-06-29 10:54:15 +0100

    Sample pointer created

--- src/hsr_testing/src/iiwa_trajectory_test.cpp
+++ src/hsr_testing/src/iiwa_trajectory_test.cpp
@@ -1,12 +1,17 @@
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
-
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <math.h>
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
 #include <thread>
+#include <Eigen/Dense>
 
-int main(int argc, char* argv[])
+#define PI 3.141592653589793238462643383279502884L /* pi */
+
+int main(int argc, char *argv[])
 {
+
   // Initialize ROS and create the Node
   rclcpp::init(argc, argv);
   auto const node = std::make_shared<rclcpp::Node>(
@@ -19,7 +24,8 @@ int main(int argc, char* argv[])
   // information about the robot's state.
   rclcpp::executors::SingleThreadedExecutor executor;
   executor.add_node(node);
-  auto spinner = std::thread([&executor]() { executor.spin(); });
+  auto spinner = std::thread([&executor]()
+                             { executor.spin(); });
 
   // Create the MoveIt MoveGroup Interface
   using moveit::planning_interface::MoveGroupInterface;
@@ -27,32 +33,57 @@ int main(int argc, char* argv[])
 
   // Construct and initialize MoveItVisualTools
   auto moveit_visual_tools =
-      moveit_visual_tools::MoveItVisualTools{ node, "iiwa_link_0", rviz_visual_tools::RVIZ_MARKER_TOPIC,
-                                              move_group_interface.getRobotModel() };
+      moveit_visual_tools::MoveItVisualTools{node, "iiwa_link_0", rviz_visual_tools::RVIZ_MARKER_TOPIC,
+                                             move_group_interface.getRobotModel()};
   moveit_visual_tools.deleteAllMarkers();
   moveit_visual_tools.loadRemoteControl();
 
   // Create a closure for updating the text in rviz
-  auto const draw_title = [&moveit_visual_tools](auto text) {
-    auto const text_pose = [] {
+  auto const draw_title = [&moveit_visual_tools](auto text)
+  {
+    auto const text_pose = []
+    {
       auto msg = Eigen::Isometry3d::Identity();
       msg.translation().z() = 1.0;
       return msg;
     }();
     moveit_visual_tools.publishText(text_pose, text, rviz_visual_tools::WHITE, rviz_visual_tools::XLARGE);
   };
-  auto const prompt = [&moveit_visual_tools](auto text) { moveit_visual_tools.prompt(text); };
+  auto const prompt = [&moveit_visual_tools](auto text)
+  { moveit_visual_tools.prompt(text); };
   auto const draw_trajectory_tool_path =
       [&moveit_visual_tools, jmg = move_group_interface.getRobotModel()->getJointModelGroup("iiwa")](
-          auto const trajectory) { moveit_visual_tools.publishTrajectoryLine(trajectory, jmg); };
+          auto const trajectory)
+  { moveit_visual_tools.publishTrajectoryLine(trajectory, jmg); };
 
   // Set a target Pose
-  auto const target_pose = [] {
+  auto const target_pose = []
+  {
     geometry_msgs::msg::Pose msg;
-    msg.orientation.w = 1.0;
-    msg.position.x = 0.5;
-    msg.position.y = 0.5;
-    msg.position.z = 0.6;
+    tf2::Quaternion q;
+
+    //Define Camera Position
+    Eigen::Vector3d c_pos(0.4, 0.5, 0.2);
+    msg.position.x = c_pos[0];
+    msg.position.y = c_pos[1];
+    msg.position.z = c_pos[2];
+
+    //Define sample point
+    Eigen::Vector3d s_pos(0.7, 0.7, 0.15);
+    Eigen::Vector3d v = c_pos - s_pos;
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //   v[i] = c_pos[i] - s_pos[i];
+    // }
+
+    q.setRPY(PI - atan(sqrt(v[0]*v[0]+v[1]*v[1])/v[2]), 0.0, acos(v[1]/(sqrt(v[0]*v[0]+v[1]*v[1]))));
+    q.normalize();
+
+    msg.orientation.x = q[0];
+    msg.orientation.y = q[1];
+    msg.orientation.z = q[2];
+    msg.orientation.w = q[3];
     return msg;
   }();
   move_group_interface.setPoseTarget(target_pose);
@@ -61,7 +92,8 @@ int main(int argc, char* argv[])
   prompt("Press 'next' in the RvizVisualToolsGui window to plan");
   draw_title("Planning");
   moveit_visual_tools.trigger();
-  auto const [success, plan] = [&move_group_interface] {
+  auto const [success, plan] = [&move_group_interface]
+  {
     moveit::planning_interface::MoveGroupInterface::Plan msg;
     auto const ok = static_cast<bool>(move_group_interface.plan(msg));
     return std::make_pair(ok, msg);
